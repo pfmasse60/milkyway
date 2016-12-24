6549c6aa3abea24560775b47a983fee178fb97cb
peterfmasseiii <pfmasse60@gmail.com>
2016-12-23 21:18:16 -0700

    Added title "Main Menu"

--- src/com/petemasse/java/Main.java
+++ src/com/petemasse/java/Main.java
@@ -1,8 +1,20 @@
 package com.petemasse.java;
 
 public class Main {
+    private String menu;
+
+    public Main() {
+        menu = "Main Menu";
+    }
 
     public static void main(String[] args) {
-        System.out.println("Main Menu");
+        Main myMain = new Main();
+        System.out.println(myMain.menu);
+
+        myMain.menu = "Hello World";
+        System.out.println(myMain.menu);
     }
+
+
 }
+
